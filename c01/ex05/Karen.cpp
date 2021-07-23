0a443dcd016613e52b1313d2fe2c88a1ba8f7588
adrien <adrien.constan@gmail.com>
2021-07-23 13:37:56 +0200

    auto-commit

--- c01/ex05/Karen.cpp
+++ c01/ex05/Karen.cpp
@@ -6,14 +6,17 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 11:24:31 by calao             #+#    #+#             */
-/*   Updated: 2021/07/23 11:33:17 by calao            ###   ########.fr       */
+/*   Updated: 2021/07/23 13:37:48 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
+#include <string>
+#include <iostream>
 
 Karen::Karen(void)
 {
+	this->fptr_debug = this->_debug;
 	return ;
 }
 
@@ -24,39 +27,34 @@ Karen::~Karen(void)
 
 void	Karen::complain(std::string level)
 {
-	int i;
-
-	for (i = 0; i < level_nb; i++)
+	if (level.compare("DEBUG") == true)
 	{
-		if (this->f_ptr[i]->id == level)
-		{
-			this->f_ptr[i]->karen_action;
-			return;
-		}
+		this->fptr_debug();
+		return;
 	}
 	std::cout << "I want to speak to manager!" << std::endl;
 	return ;
 }
 
-void		Karen::debug(void)
+void		_debug(void)
 {
 	std::cout << "I am a KAAAAAAREEEENNNNNNNNNNNNNNNNNNNNNN" << std::endl;
 	return ;
 }
 
-void		Karen::info(void)
+void		_info(void)
 {
 	std::cout << "America's number ONE! Mmmmmmmerica! Usa! Usa! USA!" << std::endl;
 	return ;
 }
 
-void		Karen::warning(void)
+void		_warning(void)
 {
 	std::cout << "If don't treat me as special customer I will start to behave like a child!" << std::endl; 
 	return ;
 }
 
-void		Karen::error(void)
+void		_error(void)
 {
 	std::cout << "You are treating me like a REGULAR customer, there must be an ERROR!!!" << std::endl;
 	return ;
--- c01/ex05/Karen.hpp
+++ c01/ex05/Karen.hpp
@@ -6,12 +6,15 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 11:20:31 by calao             #+#    #+#             */
-/*   Updated: 2021/07/23 11:22:44 by calao            ###   ########.fr       */
+/*   Updated: 2021/07/23 13:36:55 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KAREN_HPP
-#define KAREN_H
+#define KAREN_HPP
+
+#include <string>
+#include <iostream>
 
 class Karen {
 
@@ -22,8 +25,14 @@ class Karen {
 		void	complain( std::string level);
 
 	private:
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
+		void	(Karen::*fptr_debug) (void) const;
+		void	(Karen::*fptr_info) (void) const;
+		void	(Karen::*fptr_warning) (void) const;
+		void	(Karen::*fptr_error) (void) const;
+
+		void	_debug(void) const;
+		void	_info(void) const;
+		void	_warning(void) const;
+		void	_error(void) const;
 };
+#endif
