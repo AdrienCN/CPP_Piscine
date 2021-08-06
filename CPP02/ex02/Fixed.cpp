d3bae8d36c678d7e7c44217afd4bc1bd2234e32e
adrien <adrien.constan@gmail.com>
2021-08-06 09:30:49 +0200

    auto-commit

---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,155 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_n = 0;
+	return ;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed(const int src)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_n = src << Fixed::_bits; // src * 2^8
+	return ;
+}
+
+Fixed::Fixed(const float src)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_n = (roundf(src * (1 << Fixed::_bits))); // (src * (1 * 2^8);
+	return ;
+}
+
+Fixed & Fixed::operator=(Fixed const & src)
+{
+	std::cout << "Assignation constructor called" << std::endl;
+	this->_n = src.getRawBits();
+	return *this;
+}
+
+Fixed	Fixed::operator+(Fixed const & src)
+{
+	Fixed res(this->toFloat() + src.toFloat());
+	return (res);
+}
+
+Fixed	Fixed::operator-(Fixed const & src)
+{
+	Fixed res(this->toFloat() - src.toFloat());
+	return (res);
+}
+
+Fixed	Fixed::operator*(Fixed const & src)
+{
+	Fixed res(this->toFloat() * src.toFloat());
+	return (res);
+}
+
+Fixed	Fixed::operator/(Fixed const & src)
+{
+	Fixed res(this->toFloat() / src.toFloat());
+	return (res);
+}
+
+bool 	Fixed::operator<(Fixed const & src)
+{
+	if (this->_n < src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+bool 	Fixed::operator<=(Fixed const & src)
+{
+	if (this->_n <= src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+bool 	Fixed::operator>(Fixed const & src)
+{
+	if (this->_n > src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+bool 	Fixed::operator>=(Fixed const & src)
+{
+	if (this->_n >= src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+bool 	Fixed::operator==(Fixed const & src)
+{
+	if (this->_n == src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+bool 	Fixed::operator!=(Fixed const & src)
+{
+	if (this->_n != src.getRawBits())
+		return (true);
+	else
+		return (false);
+}
+
+Fixed & Fixed::operator++(void)
+{
+	this->_n += 1;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed tmp(*this);
+	this->_n += 1;
+	return tmp;
+}	
+
+int		Fixed::toInt(void) const
+{
+	return this->_n >> Fixed::_bits;
+}
+
+
+float	Fixed::toFloat(void) const
+{
+	return (float)this->_n / (1 << Fixed::_bits);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+int		Fixed::getRawBits(void) const
+{
+	return (this->_n);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_n = raw;
+}
+
+std::ostream & operator<<(std::ostream & o, Fixed const & src)
+{
+	o << src.toFloat();
+	return o;
+}
---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Fixed{
+	public:
+			Fixed();
+			Fixed(Fixed const & src);
+			Fixed(const int src);
+			Fixed(const float src);
+	Fixed &	operator=(Fixed const & src);
+			~Fixed();
+	
+	Fixed 	operator+(Fixed const & src);
+	Fixed 	operator-(Fixed const & src);
+	Fixed 	operator*(Fixed const & src);
+	Fixed 	operator/(Fixed const & src);
+
+	bool	operator>(Fixed const & src);
+	bool	operator<(Fixed const & src);
+	bool	operator>=(Fixed const & src);
+	bool	operator<=(Fixed const & src);
+	bool	operator==(Fixed const & src);
+	bool	operator!=(Fixed const & src);
+
+	Fixed & operator++(void);
+	Fixed & operator--(void);
+
+	Fixed	operator--(int);
+	Fixed	operator++(int);
+
+	static Fixed & min(Fixed const & a, Fixed const & b);
+	static Fixed & max(Fixed const & a, Fixed const & b);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+	private:
+		int		_n;
+		static const int _bits = 8;
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & src);
+//const int	Fixed::_bits = 8;
