7baf044d896334b9994b9c44b56cfc1cb8146927
adrien <adrien.constan@gmail.com>
2021-08-28 10:06:11 +0200

    auto-commit

---
+++ CPP03/ex02/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/07 11:18:25 by calao             #+#    #+#             */
+/*   Updated: 2021/08/28 00:21:50 by calao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	ScavTrap r2d2("rd2d2");
+	ScavTrap c3p0("888_" + r2d2.get_name() + "_8888");
+	r2d2.guardGate();
+	return (0);
+}
