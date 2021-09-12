0945542954f373949589c5e4b75722f619b44294
adrien <adrien.constan@gmail.com>
2021-09-12 17:15:33 +0200

    auto-commit

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,28 +1,45 @@
-#include <iostream>
 #include "mutantstack.hpp"
 
-int	main(void)
+int main()
 {
 	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	mstack.push(21);
+	mstack.push(1);
+	mstack.push(2);
 	mstack.push(3);
+	mstack.push(4);
 	mstack.push(5);
-	mstack.push(737);
-	
-	mstack.push(0);
+	mstack.push(42);
+
+	std::cout << "Element in TOP: " << mstack.top() << std::endl;
+	std::cout << "Stack SIZE: " << mstack.size() << std::endl;
+	std::cout << "Popping one element ..." << std::endl;
+	mstack.pop();
+	std::cout << "New element in TOP: " << mstack.top() << std::endl;
+	std::cout << "Stack SIZE: " << mstack.size() << std::endl;
+	std::cout << "Pushing element ..." << std::endl;
+	std::cout << "Pushing element ..." << std::endl;
+	std::cout << "Pushing element ..." << std::endl;
+	std::cout << "Pushing element ..." << std::endl;
+	mstack.push(6);
+	mstack.push(7);
+	mstack.push(8);
+	mstack.push(42);
+	std::cout << "New element in TOP: " << mstack.top() << std::endl;
+	std::cout << "Stack SIZE: " << mstack.size() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << "it: " << *it << std::endl;
 	++it;
+	std::cout << "++it: " << *it << std::endl;
 	--it;
+	std::cout << "--it: " << *it << std::endl;
+	std::cout << "stack content: " << std::endl;
 	while (it != ite)
-	{
+	{	
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::stack<int> s(mstack);
 	return 0;
 }
--- CPP08/ex02/mutantstack.hpp
+++ CPP08/ex02/mutantstack.hpp
@@ -5,12 +5,23 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
 #include <list>
 
 template <typename T>
 class MutantStack : public std::stack<T> 
 {
 	public:
+		MutantStack() : std::stack<T>() {};
+		MutantStack(MutantStack const & src) : std::stack<T>(src) {};
+		MutantStack	&	operator=(MutantStack const & src)
+		{
+			this->c = src.c ;
+			return *this;
+		}
+		virtual ~MutantStack() {};
 	
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	
@@ -18,11 +29,9 @@ class MutantStack : public std::stack<T>
 	{
 		return this->c.begin();
 	}
-	
 	iterator end()
 	{
-		return this->c.begin();
+		return this->c.end();
 	}
-	private:
 };
 #endif
